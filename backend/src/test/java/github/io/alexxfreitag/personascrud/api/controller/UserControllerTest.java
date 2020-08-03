ee072e066761e43462fe12bc4edfd37c74e436a1
Alex Eduardo Freitag <alex.f@quantich.com.br>
2020-08-02 21:19:40 -0300

    include test for invalid payload when creating a new user

--- backend/src/test/java/github/io/alexxfreitag/personascrud/api/controller/UserControllerTest.java
+++ backend/src/test/java/github/io/alexxfreitag/personascrud/api/controller/UserControllerTest.java
@@ -8,8 +8,10 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 
 import java.time.LocalDate;
@@ -25,5 +27,12 @@ public class UserControllerTest {
     public void shouldReturnOK() throws Exception {
         //User user = new User("Alex", "11111111111", "Masculino", "alex@email.com", LocalDate.now(), "Brazil", "SC");
         this.mockMvc.perform(get("/users")).andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    public void shouldNotCreateUserWithInvalidData() throws Exception {
+        User user = new User("", "", "Masculino", "alex@email.com", LocalDate.now(), "Brazil", "SC");
+        this.mockMvc.perform(post("/users")).andExpect(MockMvcResultMatchers.status().isBadRequest());
+
     }
 }
