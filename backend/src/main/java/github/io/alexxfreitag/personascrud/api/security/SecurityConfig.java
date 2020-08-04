1bbb4c5ee361e319b521cb00aa10fe03f5c415d0
Alex Eduardo Freitag <alex.f@quantich.com.br>
2020-08-03 23:26:49 -0300

    configure cors to connect with frontend

--- backend/src/main/java/github/io/alexxfreitag/personascrud/api/security/SecurityConfig.java
+++ backend/src/main/java/github/io/alexxfreitag/personascrud/api/security/SecurityConfig.java
@@ -1,19 +1,38 @@
 package github.io.alexxfreitag.personascrud.api.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
+import java.util.Collections;
+
+@Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    @Bean
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
     }
 
     @Override
@@ -35,5 +54,27 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .authenticated()
             .and().httpBasic()
         ;
+    }
+
+    @Override
+    public void configure(WebSecurity web) {
+        //web.ignoring().antMatchers(HttpMethod.GET,"/users");
+        web.ignoring().antMatchers(HttpMethod.GET,"/source");
+        web.ignoring().antMatchers(HttpMethod.OPTIONS,"/**");
+        /*web.ignoring().antMatchers(HttpMethod.POST,"/auth");*/
+    }
+
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Collections.singletonList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT",
+                "PATCH", "DELETE",
+                "OPTIONS", "HEAD",
+                "TRACE", "CONNECT"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
