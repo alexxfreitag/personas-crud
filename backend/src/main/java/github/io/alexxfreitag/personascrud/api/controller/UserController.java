f756d13adf370f52858ca126d35088f18ee93109
Alex Eduardo Freitag <alex.f@quantich.com.br>
2020-08-17 18:00:48 -0300

    move and separate all the rules from user's controller to service

--- backend/src/main/java/github/io/alexxfreitag/personascrud/api/controller/UserController.java
+++ backend/src/main/java/github/io/alexxfreitag/personascrud/api/controller/UserController.java
@@ -26,17 +26,13 @@ public class UserController implements UserResource {
     }
 
     @Override
-    public List<User> getUsers() {
-        return userRepository.findAll();
+    public ResponseEntity<List<User>> getUsers() {
+        return ResponseEntity.ok(this.userService.getUsers());
     }
 
     @Override
-    public ResponseEntity<User> getUser(UUID id) {
-        Optional<User> user = userRepository.findById(id);
-        if (user.isPresent()) {
-            return ResponseEntity.ok(user.get());
-        }
-        return ResponseEntity.notFound().build();
+    public ResponseEntity<?> getUser(UUID id) {
+        return this.userService.getUser(id);
     }
 
     @Override
@@ -44,43 +40,17 @@ public class UserController implements UserResource {
 
         user.setCreatedAt(LocalDateTime.now());
         user.setUpdatedAt(LocalDateTime.now());
-        User savedUser = this.userService.saveUser(user);
-        return ResponseEntity.ok(savedUser);
+        return this.userService.createUser(user);
     }
 
     @Override
-    public ResponseEntity<User> deleteUser(UUID id) {
-        Optional<User> user = userRepository.findById(id);
-        if (user.isPresent()) {
-            this.userRepository.delete(user.get());
-            return ResponseEntity.noContent().build();
-        }
-        return ResponseEntity.notFound().build();
+    public ResponseEntity<?> deleteUser(UUID id) {
+        return this.userService.deleteUser(id);
     }
 
     @Override
     public ResponseEntity<?> updateUser(UUID id, User user) {
-
-        return this.userRepository.findById(id).map(userData -> {
-            if (!user.getCpf().isEmpty()) {
-                if (!user.getCpf().equals(userData.getCpf())) {
-                    Optional<User> userFounded = this.userRepository.findByCpf(user.getCpf());
-                    if (userFounded.isPresent()) {
-                        throw new UserAlreadyExistsException("A user with this CPF already exists.");
-                    }
-                    userData.setCpf(user.getCpf());
-                }
-            }
-            if (!user.getName().isEmpty()) userData.setName(user.getName());
-            if (!user.getDateOfBirth().toString().isEmpty()) userData.setDateOfBirth(user.getDateOfBirth());
-            if (!user.getEmail().isEmpty()) userData.setEmail(user.getEmail());
-            if (!user.getGender().isEmpty()) userData.setGender(user.getGender());
-            if (!user.getNacionality().isEmpty()) userData.setNacionality(user.getNacionality());
-            if (!user.getNaturality().isEmpty()) userData.setNaturality(user.getNaturality());
-            userData.setUpdatedAt(LocalDateTime.now());
-            this.userRepository.save(userData);
-            return ResponseEntity.noContent().build();
-        }).orElseGet(() -> ResponseEntity.notFound().build());
+        return this.userService.updateUser(id, user);
     }
 
 }
--- backend/src/main/java/github/io/alexxfreitag/personascrud/api/service/impl/UserServiceImpl.java
+++ backend/src/main/java/github/io/alexxfreitag/personascrud/api/service/impl/UserServiceImpl.java
@@ -5,10 +5,15 @@ import github.io.alexxfreitag.personascrud.api.service.UserService;
 import github.io.alexxfreitag.personascrud.domain.model.User;
 import github.io.alexxfreitag.personascrud.domain.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
+
+import static org.springframework.http.ResponseEntity.ok;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -21,16 +26,66 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User saveUser(User user) throws UserAlreadyExistsException {
-        Optional<User> userFounded = this.userRepository.findByCpf(user.getCpf());
-        if (userFounded.isPresent()) {
-            throw new UserAlreadyExistsException("A user with this CPF already exists.");
-        }
+
         this.userRepository.save(user);
         return user;
     }
 
     @Override
+    public ResponseEntity<User> createUser(User user) throws UserAlreadyExistsException{
+        Optional<User> userFounded = this.userRepository.findByCpf(user.getCpf());
+        if (userFounded.isPresent()) {
+            throw new UserAlreadyExistsException("A user with this CPF already exists.");
+        }
+        return ResponseEntity.ok(saveUser(user));
+    }
+
+    @Override
+    public ResponseEntity<?> updateUser(UUID id, User user) throws UserAlreadyExistsException{
+
+        return this.userRepository.findById(id).map(userData -> {
+            if (!user.getCpf().isEmpty()) {
+                if (!user.getCpf().equals(userData.getCpf())) {
+                    Optional<User> userFounded = this.userRepository.findByCpf(user.getCpf());
+                    if (userFounded.isPresent()) {
+                        throw new UserAlreadyExistsException("A user with this CPF already exists.");
+                    }
+                    userData.setCpf(user.getCpf());
+                }
+            }
+            if (!user.getName().isEmpty()) userData.setName(user.getName());
+            if (!user.getDateOfBirth().toString().isEmpty()) userData.setDateOfBirth(user.getDateOfBirth());
+            if (!user.getEmail().isEmpty()) userData.setEmail(user.getEmail());
+            if (!user.getGender().isEmpty()) userData.setGender(user.getGender());
+            if (!user.getNacionality().isEmpty()) userData.setNacionality(user.getNacionality());
+            if (!user.getNaturality().isEmpty()) userData.setNaturality(user.getNaturality());
+            userData.setUpdatedAt(LocalDateTime.now());
+            return ResponseEntity.ok(saveUser(userData));
+        }).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @Override
+    public ResponseEntity<?> deleteUser(UUID id) {
+        Optional<User> user = userRepository.findById(id);
+        if (user.isPresent()) {
+            this.userRepository.delete(user.get());
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+
+    @Override
     public List<User> getUsers() {
         return this.userRepository.findAll();
+    }
+
+    @Override
+    public ResponseEntity<?> getUser(UUID id) {
+        Optional<User> user = userRepository.findById(id);
+        if (user.isPresent()) {
+            return ResponseEntity.ok(user.get());
+        }
+        return ResponseEntity.notFound().build();
     }
 }
