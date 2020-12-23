4cc49140abda9f73003bc842899839d983b86d23
Alex Eduardo Freitag <devfreitag@gmail.com>
2020-12-23 10:08:26 -0300

    add script to insert default registers when starting the application

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,19 @@
+INSERT INTO users (id, name, cpf, gender, email, date_of_birth, nacionality, naturality)
+VALUES
+    ('5950fd4e-8613-4e56-97bc-7f1e331266eb', 'Alex Freitag', '123.456.789-89', 'Male', 'alexefreitag@gmail.com',
+    parsedatetime('29-02-2000 00:00:00.000', 'dd-MM-yyyy hh:mm:ss.SS'), 'Brazil', 'SC');
+
+INSERT INTO users (id, name, cpf, gender, email, date_of_birth, nacionality, naturality)
+VALUES
+    ('0797e685-043b-48ac-9173-1a36d8bd9b33', 'Alexandre Heitor Felipe da Luz', '677.335.964-32', 'Male', 'alexandreheitorfelipedaluz-91@habby-appe.com.br',
+    parsedatetime('23-09-1985 00:00:00.000', 'dd-MM-yyyy hh:mm:ss.SS'), 'Brazil', 'RJ');
+
+INSERT INTO users (id, name, cpf, gender, email, date_of_birth, nacionality, naturality)
+VALUES
+    ('116e40b4-a208-41c2-bbe7-3772301535cb', 'Luís Márcio da Silva', '940.773.823-00', 'Male', 'luismarciodasilva__@policiapenal.com',
+    parsedatetime('15-07-1992 00:00:00.000', 'dd-MM-yyyy hh:mm:ss.SS'), 'Brazil', 'DF');
+
+INSERT INTO users (id, name, cpf, gender, email, date_of_birth, nacionality, naturality)
+VALUES
+    ('eb674cdb-7fd3-447a-b6ae-5ea473268976', 'Andreia Rita da Conceição', '689.895.001-09', 'Female', 'andreiaritadaconceicao-82@vivax.com.br',
+    parsedatetime('17-04-1983 00:00:00.000', 'dd-MM-yyyy hh:mm:ss.SS'), 'Brazil', 'CE');
